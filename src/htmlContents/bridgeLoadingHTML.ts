7fb8f6c6af979b9088390a76451b5ec35f8b8557
mohanramCypherD <mohanram@cypherd.io>
2023-02-24 23:42:46 +0530

    fix: bridge modals

--- src/htmlContents/bridgeLoadingHTML.ts
+++ src/htmlContents/bridgeLoadingHTML.ts
@@ -1,6 +1,4 @@
-export const bridgeLoadingHTML = () => {
-  return (
-    '<div class="flex flex-col rounded-[30px] bg-white h-[30%] py-[25px] w-[30%]">'+
+export const bridgeLoadingHTML = `'<div class="flex flex-col rounded-[30px] bg-white py-[25px] w-[30%]">'+
       '<div class="flex justify-end w-full px-[20px]">'+
         '<img onclick="closePopup()" src="https://public.cypherd.io/icons/close_icon.svg" class="cursor-pointer">'+
       '</div>'+
@@ -9,6 +7,4 @@ export const bridgeLoadingHTML = () => {
           '<p class="text-[16px] text-center mt-[10px]">Your transaction is being processed. This can take upto 5 mins</p>'+
           '<img class="mt-[5px]" src="https://public.cypherd.io/icons/logos/loading.gif" alt="loading gif" width="300" height="300">'+
       '</div>'+
-    '</div>'
-  )
-};
+    '</div>'`;
--- src/htmlContents/bridgeSuccessHTML.ts
+++ src/htmlContents/bridgeSuccessHTML.ts
@@ -1,34 +1,12 @@
-export const bridgeSuccessHTML = '<div id="bridge-popup-css" class="rounded-[30px] px-[50px] py-[12px] h-[35%] justify-evenly">'+
+export const bridgeSuccessHTML = `'<div id="bridge-popup-css" class="rounded-[30px] px-[50px] py-[12px] justify-evenly">'+
 '<div id="bp-heading" class="flex flex-col justify-center items-center">'+
   '<img src="https://public.cypherd.io/icons/logos/success.png" alt="successLogo" width="42" height="42">'+
   '<h2 class="text-[23px] font-semibold mt-[3px]">Congrats</h2>'+
 '</div>'+
-'<p class="text-center text-[18px] mt-[5px]">You have '+ (globalThis?.bridgeQuote?.transferAmount)?.toString() + ' ' + globalThis?.requiredTokenDetail?.chainDetails?.symbol + ' in ' + globalThis?.requiredTokenDetail?.chainDetails?.backendName + '. You can now use dapp. Switch chain back to ' + globalThis?.requiredTokenDetail?.chainDetails?.backendName + '</p>'+
-'<div id="bp-switch-container" class="30px">'+
-  '<div id="bp-switch-chain-container">'+
-    '<img src="https://public.cypherd.io/icons/logos/' + globalThis?.exchangingTokenDetail?.chainDetails?.backendName?.toLowerCase() + '.png" alt="' + globalThis?.exchangingTokenDetail?.chainDetails?.backendName + ' logo" width="42" height="42">'+
-    '<p class="text-[14px]">' + globalThis?.exchangingTokenDetail?.symbol + '</p>'+
-    '<p class="text-[16px] font-semibold">'+ globalThis?.exchangingTokenDetail?.chainDetails?.backendName +'</p>'+
-  '</div>'+
-  '<div id="bp-switch-icon-container">'+
-    '<img src="https://public.cypherd.io/icons/logos/switch_network.png" alt="switch icon" width="120" height="120">'+
-  '</div>'+
-  '<div id="bp-switch-chain-container">'+
-    '<img src="https://public.cypherd.io/icons/logos/' + globalThis?.requiredTokenDetail?.chainDetails?.backendName?.toLowerCase() + '.png" alt="' + globalThis?.requiredTokenDetail?.chainDetails?.backendName?.toLowerCase() + ' logo" width="42" height="42">'+
-    '<p class="text-[14px]">' + globalThis?.requiredTokenDetail?.symbol + '</p>'+
-    '<p class="text-[16px] font-semibold">'+ globalThis?.requiredTokenDetail?.chainDetails?.backendName +'</p>'+
-  '</div>'+
-'</div>'+
-  '<div class=" flex flex-row justify-center items-center w-[100%]">'+
+'<p class="text-center text-[18px] mt-[5px]">You have '+ (globalThis?.bridgeQuote?.transferAmount)?.toString() + ' ' + globalThis?.requiredTokenDetail?.chainDetails?.symbol + ' in ' + globalThis?.requiredTokenDetail?.chainDetails?.backendName + '. You can now use dapp.</p>'+
+
+  '<div class=" flex flex-row justify-center items-center w-[100%] mt-[50px]">'+
     '<button class="bg-[#2081E2] text-[16px] font-semibold border-none text-white h-[45px] w-[60%] rounded-[3px]" onclick="closePopup()">OK</button>'+
   '</div>'+
-'</div>';
+'</div>'`;
 
-
-// export const bridgeSuccessHTML = `'<div id="bridge-popup-css">'+
-//   '<img src="https://public.cypherd.io/icons/logos/" alt="successLogo" width="42" height="42">'+
-//   '<div id="bp-heading">'+
-//     '<h2>Congrats</h2>'+
-//   '</div>'+
-//   '<p>You have '+ (globalThis.bridgeQuote.transferAmount).toString() + ' ' + globalThis.requiredTokenDetail.chainDetails.symbol + ' in ' + globalThis.requiredTokenDetail.chainDetails.backendName + '. You can now use the dapp. </p>'+
-// '</div>'`;
