b8d29a1aa57d0e4dab0fbf58d636abc520e4364d
mohanramCypherD <mohanram@cypherd.io>
2023-03-04 11:19:15 +0530

    fix: close popup and prevent popup in mobile

--- src/scriptContents/noBalanceScript.ts
+++ src/scriptContents/noBalanceScript.ts
@@ -111,6 +111,8 @@ export const noBalanceScript = () => {
       function closePopup () {
         const popupBackground = document.getElementById("popupBackground");
         popupBackground.remove();
+        window.location.reload();
+        console.log('reload Triggered');
       }
 
       async function fetchCurrentNetwork () {
