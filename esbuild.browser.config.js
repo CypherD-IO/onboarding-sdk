fe0a5176a8f83a867b80aac259dc8b64445ae947
sedhu <sedhu@cypherd.io>
2023-03-31 17:48:33 +0530

    feat: seperate css import

--- esbuild.browser.config.js
+++ esbuild.browser.config.js
@@ -1,15 +1,31 @@
 
 const esbuild = require('esbuild');
-const cssModulesPlugin = require("esbuild-css-modules-plugin");
+// const postcss = require('esbuild-postcss');
+const postcss = require('esbuild-postcss-plugin');
+// const stylePlugin = require('esbuild-style-plugin');
+const { rimraf, rimrafSync, native, nativeSync } = require('rimraf');
 
 (async function () {
+  await rimrafSync("./dist");
   await esbuild.build({
     entryPoints: ['src/browser.ts'],
     bundle: true,
     minify: false,
     sourcemap: 'external',
-    outfile: 'dist/esbuild/browser.js',
-    plugins: [cssModulesPlugin()],
+    outfile: 'dist/esbuild/onboardingsdk.js',
+    // plugins: [postcss()],
+    plugins: [
+      postcss({
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+          // Add any additional PostCSS plugins here...
+        ],
+        inject: true, // Inject CSS into JavaScript bundle
+        minimize: true,
+        sourceMap: false,
+      }),
+    ],
   });
   console.log('esbuild.browser.config.js: Done.');
 })();
