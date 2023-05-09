91b15038fc7ea6d0d767367e80528203a0192907
mohanramCypherD <mohanram@cypherd.io>
2023-05-09 16:40:19 +0530

    prototype

---
+++ src/htmlContents/bridgeInputHTML2.ts
@@ -0,0 +1,68 @@
+declare let globalThis: any;
+// '<img onclick="backToNoBalanceHTML()" src="https://public.cypherd.io/icons/back_arrow.svg" class="cursor-pointer"/>'+
+export const bridgeInputHTML2 = () => {
+  return  `
+            <div class="bg-primaryBg rounded-[30px] z-50 flex flex-col justify-between items-center m-auto w-[90%] lg:w-[30%]" id="bridge-popup-css">
+              <div class=" bg-primaryBg rounded-t-[30px] p-5 flex flex-col justify-center items-center w-full h-full">
+                <div class="flex justify-end w-full mt-[5px] ">
+                  <img onclick="closePopup()" src="https://public.cypherd.io/icons/close_icon.svg" class="cursor-pointer">
+                </div>
+                <h2 class="font-semibold text-[28px] text-primaryText my-5 lg:my-10">Enter Token Amount</h2>
+                <h3 class="font-extrabold text-[22px] text-primaryText">USD</h3>
+                <div class="flex justify-evenly w-full items-center">
+                  <div class="rounded-full flex items-center justify-center h-[40px] w-[40px] lg:h-[40px] lg:w-[40px] bg-white cursor-pointer" id="bp-max-button" onclick="onMax()">
+                    <p class="text-[12px] text-[#2081E1]">MAX</p>
+                  </div>
+                  <div id="bp-amount-input" class="w-[200px] lg:w-[250px]">
+                    <input type="text" class="text-center focus:outline-none font-extrabold text-[70px] text-primaryText bg-primaryBg" id="bp-amount-value" placeholder="0.00" onfocus="onFocusInput(event)" onblur="onBlurInput(event)">
+                  </div>
+                  <div class="rounded-full flex items-center justify-center h-[48px] w-[48px] cursor-pointer" id="bp-switch-button">
+                  </div>
+                </div>
+                <div class="flex">
+                  <p class="mr-2 text-[18px] text-primaryText" id="bp-token-value">00</p>
+                  <p class="text-[18px] text-primaryText">${globalThis.exchangingTokenDetail.symbol}</p>
+                </div>
+                <div class="flex my-[3]">
+                  <p class="text-[14px] text-primaryText" id="bp-min-amount">Min amount: $${Math.max(10, requiredUsdValue(globalThis.requiredTokenDetail, globalThis.exchangingTokenDetail)).toFixed(2)}</p>
+                </div>
+                <div class="bg-primaryBg border border-[#E4E4E4] p-2 flex rounded-2xl w-[90%] lg:w-[75%] mt-6">
+                  <img src="${globalThis.exchangingTokenDetail.logoUrl}" alt="${globalThis.exchangingTokenDetail.logoUrl}"
+                  class="h-[45px] w-[45px] lg:h-[55px] lg:w-[55px] rounded-lg">
+                  <div class="w-full ml-3" id="bp-balance-detail">
+                    <div id="bp-balance-detail-usd" class="flex justify-between h-[25px]">
+                      <p class="text-primaryText text-[18px] font-semibold">${globalThis.exchangingTokenDetail.chainDetails.backendName}</p>
+                      <p class="text-primaryText text-[18px] font-semibold" id="bp-balance-detail-usd-value">$${(globalThis.exchangingTokenDetail.actualBalance * globalThis.exchangingTokenDetail.price).toFixed(2)}</p>
+                    </div>
+                    <div id="bp-balance-detail-token" class="flex justify-between h-[25px]">
+                      <p class="text-[#929292] text-[16px] font-normal">${globalThis.exchangingTokenDetail.symbol}</p>
+                      <p class="text-[#929292] text-[16px] font-normal" id="bp-balance-detail-token-value">${parseFloat(globalThis.exchangingTokenDetail.actualBalance).toFixed(6)}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div id="bp-submit-button-container" class="bg-primaryBg py-5 lg:py-10 w-full flex items-center justify-center rounded-b-[30px]">
+                <button class="bg-[#2081E2] w-[60%] py-4 rounded-lg text-white text-[16px] font-semibold" onclick="bridgeSubmitConditionCheck(globalThis.exchangingTokenDetail.chainDetails.chain_id, globalThis.exchangingTokenDetail.chainDetails.backendName)">Submit</button>
+              </div>
+              <div class="flex flex-row justify-between w-[100%] py-[25px] px-[20px] bg-[#3C4143] rounded-b-[30px] mt-[25px]">
+                <a class="flex flex-row items-center text-[14px] text-white" href=${globalThis.cypherWalletUrl} target="_blank">
+                  <img src="https://public.cypherd.io/icons/logos/cypher.png" class="ml-[10px] mr-[3px] w-[18px]" alt="Arbitrum logo" resizeMode="contain"> Cypher Wallet
+                </a>
+                <div class="h-[35px] w-[50%] flex flex-row justify-end">
+                  <div class="h-[35px] w-[70px] flex flex-row">
+                    <div class="toggle-switch">
+                        <label>
+                            <input onclick="switchTheme()" class="toggle-input" type="checkbox">
+                            <span class="slider"></span>
+                        </label>
+                    </div>
+                  </div>
+                  <div class="relative w-[55px] ml-[20px] flex items-center">
+                    <img id="chat-support" class="cursor-pointer" src="https://public.cypherd.io/icons/chat.png" onclick="openChat()">
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+}
---
+++ src/htmlContents/noBalance2.ts
@@ -0,0 +1,211 @@
+import _ from "lodash";
+import { bridgePopup } from "../utils/bridgePopup";
+
+declare let globalThis: any;
+
+function __capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export const noBalanceHTML2 = (parent: any, totalHoldings: any, showInfoScreen: boolean) => {
+
+  const bridgeableTokensList: any = {};
+  totalHoldings?.map((tokenDetail: any) => { if (tokenDetail.actualBalance * tokenDetail.price >= 10 && tokenDetail.isVerified) { bridgeableTokensList[(tokenDetail.name).toLowerCase()] = tokenDetail } });
+  console.log('totalt olding after check : ', Object.values(bridgeableTokensList));
+  const tokensAvailableList = Object.values(bridgeableTokensList).map((tokenDetail: any) => `
+    <tr class="odd:bg-stripedTableBg h-[75px]">
+      <td class="pl-2">
+        <div id="cyd-chain">
+          <img id="td-chain-icon" class="w-[20px] mr-1" src="https://public.cypherd.io/icons/logos/${_.get(tokenDetail, ['chainDetails', 'backendName'], '').toLowerCase()}.png" alt="${_.get(tokenDetail, ['chainDetails', 'backendName'], '').toLowerCase()} logo" />
+          <p class='text-[10px] lg:text-[14px] text-primaryText'>${_.get(tokenDetail, ['chainDetails', 'backendName'], '')}</p>
+        </div>
+      </td>
+      <td>
+        <div id="cyd-token">
+          <img id="td-token-icon" class="w-[20px] mr-1" src="${_.get(tokenDetail, ['logoUrl'])}" alt="${_.get(tokenDetail, ['name'])} logo">
+          <p id="td-token-name" class='text-[10px] lg:text-[14px] text-primaryText'>${_.get(tokenDetail, ['name'])}</p>
+        </div>
+      </td>
+      <td>
+        <p id="td-usd-value" class='text-[10px] lg:text-[14px] text-primaryText font-semibold'>$ ${(_.get(tokenDetail, ['actualBalance']) * _.get(tokenDetail, ['price'])).toFixed(2)}</p>
+      </td>
+      <td>
+        <p id="td-token-balance" class='text-[10px] lg:text-[14px] text-primaryText'>${Number(_.get(tokenDetail, ['actualBalance'])).toFixed(5)}</p>
+      </td>
+      <td class="pr-2">
+        <button class="blue-button text-[10px] lg:text-[14px] text-primaryText p-1.5 lg:p-3">Exchange</button>
+      </td>
+    </tr>
+  `).join(' ');
+
+  console.log('tokenAvailable List : ', tokensAvailableList);
+
+  // const parentDiv = document.createElement('div');
+  // parentDiv.innerHTML = tokensAvailableList;
+  // console.log('tokenDetail', tokenDetail);
+
+
+  // const updatedTokensAvailableList = div.innerHTML;
+
+  const tokenListContainer = Object.keys(bridgeableTokensList).length ? `
+  <div class="w-[100%] max-h-[60%] flex flex-col justify-center items-center">
+    <p class='text-[16px] text-primaryText font-semibold mb-[20px] px-10 lg:px-0 text-center'>
+      You can exchange with below tokens in your wallet
+    </p>
+    <div id="tokens-available-flex-box" class="w-[90%] lg:w-[80%] mt-2 lg:mt-0">
+      <table class="w-[100%]" id="tokens-list-table">
+        ${tokensAvailableList}
+      </table>
+    </div>
+  </div>` : `<div class="w-[100%] flex flex-col justify-center items-center">
+    <p class='text-[16px] text-primaryText font-semibold mb-[20px]'>Insufficient funds to perform this action!</p>
+    <img src="https://public.cypherd.io/icons/emptyWallet.png" class="h-[200px] w-[100px] lg:h-[300px] lg:w-[200px]">
+  </div>
+  `
+
+  const htmlValue = `
+    <div
+      id="popup"
+      class="flex flex-col items-center justify-between max-h-[85%] rounded-[30px] bg-primaryBg w-11/12 lg:w-3/5 ${!showInfoScreen || !Object.keys(bridgeableTokensList).length ? 'block' : 'hidden'}"
+    >
+      <div class="flex flex-row justify-end w-[95%] mt-[20px] mx-[30px] bg-primaryBg">
+        <img onclick="closePopup()" src="https://public.cypherd.io/icons/close_icon.svg" class="cursor-pointer">
+      </div>
+      <div class="my-[20px] px-10 lg:px-0 text-center">
+        <span class="text-[23px] text-primaryText font-semibold float-left">You need</span>
+        <img
+          src="${_.get(globalThis.requiredTokenDetail, ["logoUrl"])}"
+          alt="Arbitrum logo"
+          class="w-[32px] h-[32px] mx-[8px] float-left"
+        />
+        <span class="text-[23px] text-primaryText font-semibold float-left">${_.get(globalThis.requiredTokenDetail, ["symbol"]).toUpperCase()} in</span>
+        <img
+          src="https://public.cypherd.io/icons/logos/${_.get(globalThis.requiredTokenDetail, ["chainDetails", "backendName"]).toLowerCase()}.png"
+          alt="${_.get(globalThis.requiredTokenDetail, ["chainDetails", "backendName"]).toLowerCase()} logo"
+          class="w-[32px] h-[32px] mx-[8px] float-left"
+        />
+        <span class="text-[23px] text-primaryText font-semibold">${__capitalize(_.get(globalThis.requiredTokenDetail, ["chainDetails", "backendName"]).toLowerCase())} chain to use this dApp</span>
+      </div>
+      ${tokenListContainer}
+      <div class="flex flex-row justify-between items-center w-[100%] py-[25px] px-[20px] bg-[#3C4143] rounded-b-[30px] mt-[25px]">
+        <a class="flex flex-row items-center flex-wrap text-[14px] text-white" href=${globalThis.cypherWalletUrl} target="_blank">
+          <div class="mr-[5px]">Powered by</div>
+          <div class="flex flex-row items-center">
+          <img src="https://public.cypherd.io/icons/logos/cypher.png" class="w-[18px] mr-[5px]" alt="Cypher logo" resizeMode="contain">
+          Cypher Wallet
+          </div>
+        </a>
+        <div class="h-[35px] w-[50%] flex flex-row justify-end">
+          <div class="h-[35px] w-[70px] flex flex-row">
+            <div class="toggle-switch">
+                <label>
+                    <input onclick="switchTheme()" class="toggle-input" type="checkbox">
+                    <span class="slider"></span>
+                </label>
+            </div>
+          </div>
+          <div class="relative w-[55px] ml-[20px] flex items-center">
+            <img id="chat-support" class="cursor-pointer" src="https://public.cypherd.io/icons/chat.png" onclick="openChat()">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+    class="rounded-[30px] z-50 m-auto bg-primaryBg w-[90%] lg:w-[40%] justify-between items-center ${Object.keys(bridgeableTokensList).length && showInfoScreen ? 'block' : 'hidden'}"
+    id="bridge-info"
+    >
+      <div class=" bg-primaryBg] rounded-t-[30px] p-5 flex flex-col justify-start items-center w-full">
+        <div class="flex justify-end w-full ">
+          <img onclick="closePopup()" src="https://public.cypherd.io/icons/close_icon.svg" class="cursor-pointer">
+        </div>
+        <div id="bp-heading">
+          <h2 class="font-semibold text-[28px] my-5 text-primaryText text-center">
+            You need to bridge first
+          </h2>
+        </div>
+        <div class="flex items-start justify-evenly w-[95%] my-5">
+          <div id="bp-switch-chain-container">
+            <img src="https://public.cypherd.io/assets/dapps/unknownToken.png" class="w-[42px] h-[42px]" alt="">
+            <p class="text-[#929292] font-normal text-[16px] text-center mt-2 mb-1">Any Token</p>
+            <p class="text-primaryText font-semibold text-[18px] text-center">Any Chain</p>
+          </div>
+          <div id="bp-switch-icon-container">
+            <img src="https://public.cypherd.io/icons/logos/switch_network.png" alt="switch icon" class="w-[100px] h-[100px]">
+          </div>
+          <div id="bp-switch-chain-container">
+            <img src="${_.get(globalThis.requiredTokenDetail, ['logoUrl'])}" class="w-[42px] h-[42px]" alt="">
+            <p class="text-[#929292] font-normal text-[16px] text-center mt-2 mb-">
+            ${_.get(globalThis.requiredTokenDetail, ['symbol'])}
+            </p>
+            <p class="text-primaryText font-semibold text-[18px] text-center">
+            ${_.get(globalThis.requiredTokenDetail, ['chainDetails', 'backendName'])}
+            </p>
+          </div>
+        </div>
+        <p class="text-[20px] text-primaryText text-center mt-[10px]">
+          You dont have enough ${_.get(globalThis.requiredTokenDetail, ['symbol'])} in ${_.get(globalThis.requiredTokenDetail, ['chainDetails', 'backendName'])} chain to use this dApp
+        </p>
+      </div>
+
+      <div class="bg-primaryBg py-5 w-full flex items-center justify-center rounded-b-[30px]">
+        <button class="bg-[#2081E2] py-4 w-[60%] rounded-lg text-white text-[16px] font-semibold"
+        onclick="document.getElementById('bridge-info').style.display = 'none';document.getElementById('popup').style.display = 'flex';">
+          Continue
+        </button>
+      </div>
+
+      <div class='flex flex-row justify-between items-center w-[100%] py-[25px] px-[20px] bg-[#3C4143] rounded-b-[30px] mt-[25px]'>
+        <a class='flex flex-row items-center flex-wrap text-[14px] text-white' href=${_.get(globalThis, "cypherWalletUrl")} target="_blank">
+          <div class="mr-[5px]">Powered by</div>
+          <div class='flex flex-row items-center'>
+          <img src="https://public.cypherd.io/icons/logos/cypher.png" class="w-[18px] mr-[5px]" alt="Cypher logo" resizeMode="contain">
+          Cypher Wallet
+          </div>
+        </a>
+        <div class="h-[35px] w-[50%] flex flex-row justify-end">
+          <div class="h-[35px] w-[70px] flex flex-row">
+            <div class="toggle-switch">
+                <label>
+                    <input onclick="switchTheme()" class="toggle-input" type="checkbox">
+                    <span class="slider"></span>
+                </label>
+            </div>
+          </div>
+          <div class="relative w-[55px] ml-[20px] flex items-center">
+            <img id="chat-support" class="cursor-pointer" src="https://public.cypherd.io/icons/chat.png" onclick="openChat()">
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  parent.innerHTML = htmlValue;
+  // const button = document.getElementById('no-balance-button');
+  // console.log('button', button);
+  // button.addEventListener('click', () => {
+  //   console.log('click is triggered');
+  //   // const tokenDetail = _.omit(tokenDetail, ['about']);
+  //   bridgePopup(JSON.stringify({"name":"Arbitrum","symbol":"ARB","logoUrl":"https://public.cypherd.io/assets/blockchains/arbitrum/assets/0x912CE59144191C1204E64559FE8253a0e49E6548/logo.png","price":1.1175300565679478,"contractAddress":"0x912ce59144191c1204e64559fe8253a0e49e6548","balance":"1025000000000000000000","contractDecimals":18,"totalValue":1145.4683079821464,"actualBalance":1025,"isVerified":true,"coinGeckoId":"arbitrum","chainDetails":{"chainName":"ethereum","name":"Arbitrum One","symbol":"ETH","id":10,"backendName":"ARBITRUM","chain_id":"0xa4b1","native_token_address":"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee","chainIdNumber":42161}}));
+  // });
+  const tokenListGroup = document.getElementById("tokens-list-table");
+
+  const buttonPressed = (e: any) => {
+    console.log('target details', e.target);
+    if (e.target.nodeName === 'BUTTON') {
+      console.log('pressed is a button : ', (e.target.parentNode.parentNode).querySelector("#td-token-name").innerHTML.toLowerCase());
+      globalThis.exchangingTokenDetail = bridgeableTokensList[(e.target.parentNode.parentNode).querySelector("#td-token-name").innerHTML.toLowerCase()];
+    }
+    bridgePopup();
+  }
+
+  // if (rows) {
+  //   Array.from(rows).forEach((row, index) => {
+  //     const button = row.querySelector('button');
+  //     button.addEventListener('click', () => {
+  //       bridgePopup(_.omit(totalHoldings[index], ['about']));
+  //     });
+  //   });
+  // }
+
+  tokenListGroup?.addEventListener("click", buttonPressed);
+
+};
