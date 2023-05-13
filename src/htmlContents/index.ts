db495ee5534b4411f72434c1424b11489358fbfc
mohanramCypherD <mohanram@cypherd.io>
2023-05-13 23:00:49 +0530

    removed html contents

--- src/htmlContents/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './noBalanceHTML';
-export * from './bridgeInputHTML';
-export * from './bridgeSummaryHTML';
-export * from './bridgeSwitchHTML';
-export * from './bridgeLoadingHTML';
-export * from './switchBackHTML';
-export * from './bridgeSuccessHTML';
