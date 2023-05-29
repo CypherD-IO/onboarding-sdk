3fcd2dbfe9e1fbc493ec18bd7d12af1d0e6708de
sandeepcypher <sandeep@cypherd.io>
2023-05-29 16:47:42 +0530

    dark mode check

--- cypress/e2e/footer.cy.ts
+++ cypress/e2e/footer.cy.ts
@@ -19,30 +19,23 @@ describe('To test if footer section is rendered conditionally' , ()=> {
     .should('contain', 'https://www.cypherwallet.io');
   });
 
-
-  it.only('should toggle dark mode when the toggle label is clicked', () => {
-    cy.getByClass('close-popup').click();
-    cy.wait(1000);
-    cy.getById("addPopup").click();
-
-    cy.intercept('GET', '**/portfolio/balances**').as('fetchPortfolioBalances');
-    cy.intercept('GET', '**/swap/evm/chains').as('swapChainsCheck');
-    cy.intercept('GET', '**/swap/evm/chains/**').as('swapTokensCheck');
-
-    cy.wait('@fetchPortfolioBalances', { timeout: 50000 });
-    cy.wait('@swapChainsCheck', { timeout: 50000 });
-    cy.wait('@swapTokensCheck', { timeout: 50000 });
-    // Click the toggle label with class "toggle-input"
-    cy.get('.toggle-switch').click();
-
-    // Assert that the dark mode is enabled
-    cy.get('body').should('have.class', 'dark');
-
-    // Click the toggle label again
-    cy.get('.toggle-switch').click();
-
-    // Assert that the dark mode is disabled
-    cy.get('body').should('not.have.class', 'dark');
+  it('should toggle dark mode when the toggle label is clicked', () => {
+    cy.get('#bridge-info').then(($el) => {
+      const bgColor = $el.css('background-color');
+      const isDarkMode = bgColor === 'rgb(22, 22, 25)';
+      const isLightMode = bgColor === 'rgb(255, 255, 255)';
+      expect(isDarkMode).to.be.true;
+      cy.get('.toggle-input').click({force:true});
+      cy.get('#bridge-info').should(($el) => {
+        const newBgColor = $el.css('background-color');
+        if (isDarkMode) {
+          expect(newBgColor).to.equal('rgb(255, 255, 255)');
+        } else if (isLightMode) {
+          expect(newBgColor).to.equal('rgb(22, 22, 25)');
+        }
+      });
+    });
   });
+
   });
 
