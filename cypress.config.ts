ea755efd901ff6f84b2e43e4b883010745855571
mohanramCypherD <mohanram@cypherd.io>
2023-06-09 17:21:08 +0530

    fix: check replacement

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    projectId: "s47waj",
     baseUrl: 'http://127.0.0.1:8080/sample',
     setupNodeEvents(on, config) {
       // implement node event listeners here
