472546196bfd03bcfede72ab5a78493678f4e50b
mohanramCypherD <mohanram@cypherd.io>
2023-06-09 13:06:51 +0530

    feat: run server locally for test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://public-dev.cypherd.io/sdk',
+    baseUrl: 'http://127.0.0.1:8080/sample/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
