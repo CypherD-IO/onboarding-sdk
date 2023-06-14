258ab46ecc37c9b272f0e50dab538f27d2a4c495
mohanramCypherD <mohanram@cypherd.io>
2023-06-14 12:30:59 +0530

    test: check intercept

--- cypress/e2e/bridgeInput.cy.ts
+++ cypress/e2e/bridgeInput.cy.ts
@@ -10,9 +10,9 @@ describe('To check if bridge input screen is rendered fine ', () => {
 
     cy.intercept('GET', '**/portfolio/balances**').as('fetchPortfolioBalances');
     cy.getById("addPopup").click();
-    cy.intercept('GET', '**/swap/evm/chains').as('swapChainsCheck');
     cy.wait('@fetchPortfolioBalances', { timeout: 50000 });
-    cy.wait('@swapChainsCheck', { timeout: 50000 });
+    cy.intercept('GET', '**/swap/evm/chains').as('swapChainsCheck');
+        cy.wait('@swapChainsCheck', { timeout: 50000 });
 
     cy.getByClass('exchange-token-button').eq(0).click()
   })
