292234de092b9a825d4c6d2614f3dcb29471099d
mohanramCypherD <mohanram@cypherd.io>
2023-06-14 15:33:10 +0530

    set video false

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     viewportHeight: 660,
     viewportWidth: 1000,
     projectId: "u8y7y4",
+    video: false,
     baseUrl: 'http://127.0.0.1:8080/test',
     setupNodeEvents(on, config) {
       // implement node event listeners here
