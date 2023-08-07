b6c2d9f8b5321ce125fb458a5a006dca59e8597a
shan-c <shanthan@cypherd.io>
2023-08-07 15:27:53 +0530

    changed tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,19 @@ module.exports = {
         appBg: "var(--theme-appBg)",
         soapstoneBg: "var(--theme-soapStoneIcon)",
       },
+      screens: {
+        sm: "var(--cyd-sm)", // Small screens
+        md: "var(--cyd-md)", // Medium screens
+        lg: "var(--cyd-lg)", // Large screens
+        xl: "var(--cyd-xl)", // Extra-large screens
+      },
     },
+    // screens: {
+    //   sm: "640px", // Small screens
+    //   md: "768px", // Medium screens
+    //   lg: "1024px", // Large screens
+    //   xl: "1280px", // Extra-large screens
+    // },
   },
   plugins: [],
 };
