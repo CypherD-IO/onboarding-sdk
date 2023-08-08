61cd526d296fd0083a37d2eb782e35209e3b873d
shan-c <shanthan@cypherd.io>
2023-08-08 21:05:06 +0530

    Added prefix to tailwind classes

--- esbuild.browser.config.js
+++ esbuild.browser.config.js
@@ -1,26 +1,25 @@
-
-const esbuild = require('esbuild');
+const esbuild = require("esbuild");
 const cssModulesPlugin = require("esbuild-css-modules-plugin");
 // const postcss = require('esbuild-postcss');
-const postcss = require('esbuild-postcss-plugin');
+const postcss = require("esbuild-postcss-plugin");
 // const stylePlugin = require('esbuild-style-plugin');
-const { rimraf, rimrafSync, native, nativeSync } = require('rimraf');
+const { rimraf, rimrafSync, native, nativeSync } = require("rimraf");
 
 (async function () {
   await rimrafSync("./dist");
   await esbuild.build({
-    entryPoints: ['src/browser.ts'],
+    entryPoints: ["src/browser.ts"],
     bundle: true,
     minify: true,
-    sourcemap: 'external',
-    outfile: 'dist/esbuild/onboardingsdk.js',
+    sourcemap: "external",
+    outfile: "dist/esbuild/onboardingsdk.js",
     // plugins: [cssModulesPlugin()],
     // plugins: [postcss()],
     plugins: [
       postcss({
         plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
+          require("tailwindcss"),
+          // require('autoprefixer'),
           // Add any additional PostCSS plugins here...
         ],
         inject: true, // Inject CSS into JavaScript bundle
@@ -29,5 +28,5 @@ const { rimraf, rimrafSync, native, nativeSync } = require('rimraf');
       }),
     ],
   });
-  console.log('esbuild.browser.config.js: Done.');
+  console.log("esbuild.browser.config.js: Done.");
 })();
